45f0117c9b933fa6f00af1a0a505e51b26c1a7ee
dangrt5 <randy.dang05@gmail.com>
2018-12-14 18:06:51 -0800

    initial commit

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  // const randy = { age: 24, drives: true };
+  // res.json(randy);
+  res.send(req.query.name);
 });
 
 module.exports = router;
