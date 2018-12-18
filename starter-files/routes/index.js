bb06f3dea659ac8d9544fcc5a32ff8f86bae078c
dangrt5 <randy.dang05@gmail.com>
2018-12-17 19:29:40 -0800

    routes added and templating added

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  const randy = { name: "randy", age: 24, drive: true };
+  // res.json(randy);
+  res.render("hello");
+});
+
+router.get("/reverse/:name", (req, res) => {
+  const reverse = [...req.params.name].reverse().join("");
+  res.send(reverse);
 });
 
 module.exports = router;
